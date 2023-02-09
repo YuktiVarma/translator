e7244896e6782d65e1f000843add175af3cac0ba
YuktiVarma <124858841+YuktiVarma@users.noreply.github.com>
2023-02-09 10:46:00 +0530

    Update Translator.py

--- Translator.py
+++ Translator.py
@@ -2,4 +2,4 @@ pip install -U deep-translator
 from deep_translator import MyMemoryTranslator
 word=input("Enter the word : ")
 translated = MyMemoryTranslator(source="english", target="hindi").translate(text=word)
-translated
+
